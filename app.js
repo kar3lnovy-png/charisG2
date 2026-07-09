00a7dd457448144884e8c9c46ca3c9f8505d3e0d
kar3lnovy-png <kar3l.novy@gmail.com>
2026-07-09 20:39:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const PRODUCTS = {
     { name: "ZDARMA snížené jednodenní", price: 0 },
     { name: "ZDARMA snížené celokonferenční", price: 0 },
   ],
+  "Dary": []
 };
 
 // =================== STAV APLIKACE ===================
